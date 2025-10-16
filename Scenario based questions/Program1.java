3b1b79fd5562821b3a3e81966206c272e56d578e
Abhinav Anand <viabhavdubeyvaibhav@gmail.com>
2025-10-16 23:16:07 +0530

    Update Program1.java

--- Scenario based questions/Program1.java
+++ Scenario based questions/Program1.java
@@ -1,1 +1,19 @@
+// SCENARIO BASED QUESTION- ATM DISPENSES CASH 
 
+class ATM{
+    cash dispense(){
+        System.out.println("ATM Dispences cash");
+        cash c = new cash();
+        return c;
+    }
+    
+    public static void main(String[] args) {
+        ATM A = new ATM();
+        cash sh = A.dispense();
+        System.out.println(sh);
+    }
+}
+
+class cash {
+    
+}
