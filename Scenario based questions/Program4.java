9ca41d86fa4815a10cbea61e3925e606cec29dfc
Abhinav Anand <viabhavdubeyvaibhav@gmail.com>
2025-10-16 23:36:26 +0530

    Update Program4.java

--- Scenario based questions/Program4.java
+++ Scenario based questions/Program4.java
@@ -1,1 +1,24 @@
+// SCENARIO BASED QUESTION- DANCER DANCES AND THROWS JACKET
 
+class DANCER{
+    void dance(){
+         System.out.print("Dancer dances ");
+    }
+    
+    JACKET throwsjacket(){
+        System.out.println(" and throws jacket");
+       JACKET J = new JACKET();
+       return J;
+    }
+    
+    public static void main(String[] args) {
+        DANCER D = new DANCER();
+        D.dance();
+        JACKET JCK = D.throwsjacket();
+        System.out.println(JCK);
+    }
+}
+
+class JACKET {
+    
+}
