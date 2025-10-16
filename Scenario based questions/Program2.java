d073b52ecac6e864cc050977f70b0aba1da4a035
Abhinav Anand <viabhavdubeyvaibhav@gmail.com>
2025-10-16 23:20:52 +0530

    Implement COW class with gives method returning MILK

--- Scenario based questions/Program2.java
+++ Scenario based questions/Program2.java
@@ -1,1 +1,18 @@
+// SCENARIO BASED QUESTION- COW GIVES MILK
+class COW{
+    MILK gives(){
+        System.out.println("Cow gives Milk");
+        MILK c = new MILK();
+        return c;
+    }
+    
+    public static void main(String[] args) {
+        COW C = new COW();
+        MILK M= C.gives();
+        System.out.println(M);
+    }
+}
 
+class MILK {
+    
+}
