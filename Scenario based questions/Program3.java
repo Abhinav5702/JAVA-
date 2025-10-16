5ffc19ccbe29c010977f3a3816bd297c5796f435
Abhinav Anand <viabhavdubeyvaibhav@gmail.com>
2025-10-16 23:27:20 +0530

    Update Program3.java

--- Scenario based questions/Program3.java
+++ Scenario based questions/Program3.java
@@ -1,1 +1,18 @@
+// SCENARIO BASED QUESTION- CONDUCTOR PROVIDES TICKET
+class Conductor{
+    TICKET Provides(){
+        System.out.println("Conductor Provides Ticket");
+        TICKET t = new TICKET();
+        return t;
+    }
+    
+    public static void main(String[] args) {
+        Conductor C = new Conductor();
+        TICKET TIC= C.Provides();
+        System.out.println(TIC);
+    }
+}
 
+class TICKET {
+    
+}
