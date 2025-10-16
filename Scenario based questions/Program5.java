39d52dea6fee6137e0709a248940d45ab877ad90
Abhinav Anand <viabhavdubeyvaibhav@gmail.com>
2025-10-17 00:30:51 +0530

    Update Program5.java

--- Scenario based questions/Program5.java
+++ Scenario based questions/Program5.java
@@ -1,1 +1,21 @@
+// SCENARIO BASED QUESTION- ATM DISPENSES CASH USING PIN
 
+class ATM  {
+    CASH DISPENSES(int pin){
+        System.out.println("ATM DISPENCES CASH USING THE PIN "+ pin);
+        CASH C = new CASH();
+        return C;
+        
+    }
+    
+    
+    public static void main(String[] args) {
+       ATM A = new ATM();
+       A.DISPENSES(2255);
+       System.out.println( A.DISPENSES(2255));
+    }
+}
+
+class CASH{
+    
+}
